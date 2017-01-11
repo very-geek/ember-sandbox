6aba9a1ccccb08be7e5d9f9b76471427f5dce06d
Albert Yu <dev.yufan@gmail.com>
2017-01-11 23:36:18 +0800

    Update ember-intl to v2.17.1

--- config/ember-intl.js
+++ config/ember-intl.js
@@ -24,7 +24,7 @@ module.exports = function(/* env */) {
     * @type {String?}
     * @default "null"
     */
-    baseLocale: 'en-us',
+    baseLocale: null,
 
     /**
     * autoPolyfill, when true will automatically inject the IntlJS polyfill

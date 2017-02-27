136c5bb7addb5ca226b2a22c886d5bf3dd92555c
nightire <nightire@very-geek.com>
2017-02-27 10:03:40 +0800

    Add more examples

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,24 +11,41 @@ export default Route.extend({
   model() {
     // return this.get('ajax').request('/users')
 
-    return [
-      {x: new Date(2017, 0, 1), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 2), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 3), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 4), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 5), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 6), y: generateRandomInteger(0, 100)},
-      {x: new Date(2017, 0, 7), y: generateRandomInteger(0, 100)},
-    ]
+    // return {
+    //   series: [
+    //     {x: new Date(2017, 0, 1), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 2), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 3), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 4), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 5), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 6), y: generateRandomInteger(0, 100)},
+    //     {x: new Date(2017, 0, 7), y: generateRandomInteger(0, 100)},
+    //   ]
+    // }
 
-    // return [
-    //   {x: 'Alice', y: generateRandomInteger(0, 100)},
-    //   {x: 'Billy', y: generateRandomInteger(0, 100)},
-    //   {x: 'Cindy', y: generateRandomInteger(0, 100)},
-    //   {x: 'David', y: generateRandomInteger(0, 100)},
-    //   {x: 'Emily', y: generateRandomInteger(0, 100)},
-    //   {x: 'Fayez', y: generateRandomInteger(0, 100)},
-    //   {x: 'Grace', y: generateRandomInteger(0, 100)},
-    // ]
+    // return {
+    //   series: [
+    //     {x: 'Alice', y: generateRandomInteger(0, 100)},
+    //     {x: 'Billy', y: generateRandomInteger(0, 100)},
+    //     {x: 'Cindy', y: generateRandomInteger(0, 100)},
+    //     {x: 'David', y: generateRandomInteger(0, 100)},
+    //     {x: 'Emily', y: generateRandomInteger(0, 100)},
+    //     {x: 'Fayez', y: generateRandomInteger(0, 100)},
+    //     {x: 'Grace', y: generateRandomInteger(0, 100)},
+    //   ]
+    // }
+
+    return {
+      series: [
+        {x: new Date(2017, 0, 1), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 2), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 3), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 4), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 5), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 6), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+        {x: new Date(2017, 0, 7), 'Alice': generateRandomInteger(0, 100), 'Billy': generateRandomInteger(0, 100), 'Cindy': generateRandomInteger(0, 100)},
+      ],
+      columns: ['Alice', 'Billy', 'Cindy']
+    }
   }
 })
